61b99c82c77b0687ce43998d1b08f2de4a769e08
YongsakSaengsuk <Yongsaksaengsuk@gmail.com>
2024-12-20 20:11:20 +0700

    button

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,13 +1,22 @@
 import "./Item.css";
 export default function Item(porps) {
   const { text, Checkitem, Edittask } = porps;
+  const handleCheckboxChange = () => {
+    Checkitem(text.id);  
+  };
   return (
     <div className={`item ${text.check ? "diabled" : ""}`}>
       <h2>{text.text}</h2>
       <div className="btn">
-        <button className="edit-btn" onClick={() => Edittask(text.id)}>Edit</button>
+        <button className="edit-btn" onClick={() => Edittask(text.id)}>
+          Edit
+        </button>
         <label>
-          <input type="checkbox" onClick={() => Checkitem(text.id)} checked={text.check}/ >
+          <input
+            type="checkbox"
+            onChange={handleCheckboxChange}
+            checked={text.check}
+          />
         </label>
       </div>
     </div>
